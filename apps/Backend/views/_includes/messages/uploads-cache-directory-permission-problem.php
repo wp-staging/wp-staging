5e2046f089d892a931d50dd567145766dbf61798
Ilgıt Yıldırım <ilgityildirim@gmail.com>
2017-03-05 12:54:34 +0100

    Small fix

--- apps/Backend/views/_includes/messages/uploads-cache-directory-permission-problem.php
+++ apps/Backend/views/_includes/messages/uploads-cache-directory-permission-problem.php
@@ -1,10 +1,10 @@
 <div class="error">
     <p>
         <strong>WP Staging Folder Permission error: </strong>
-        <?php echo ABSPATH . "wp-content/uploads/" . WPStaging::SLUG?>
+        <?php echo ABSPATH . "wp-content/uploads/" . \WPStaging\WPStaging::SLUG?>
         is not write and/or readable.
         <br>
-        Check if the folder <strong><?php echo ABSPATH . "wp-content/uploads/" . WPStaging::SLUG?></strong> exists!
+        Check if the folder <strong><?php echo ABSPATH . "wp-content/uploads/" . \WPStaging\WPStaging::SLUG?></strong> exists!
         File permissions should be chmod 755 or 777.
     </p>
 </div>
