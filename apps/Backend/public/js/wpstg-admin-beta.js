6c512f2e29711be7c75859d56ebc66a119a45f55
René Hermenau <admin@xsimulator.net>
2017-05-09 12:28:25 +0200

    fix close beta message button

--- apps/Backend/public/js/wpstg-admin-beta.js
+++ apps/Backend/public/js/wpstg-admin-beta.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){
-    $(".wpstg_hide_poll").click(function(e) {
+    $(".wpstg_hide_beta").click(function(e) {
         e.preventDefault();
 
         WPStaging.ajax(
@@ -18,38 +18,5 @@ jQuery(document).ready(function($){
                 );
             }
         );
-
-        // var url = $(this).data("url");
-        //
-        // $.ajax({
-        //     url     : url,
-        //     type    : "POST",
-        //     data    : {action: "wpstg_hide_beta"},
-        //     dataType: "JSON",
-        //     error       : function(xhr, textStatus, errorThrown) {
-        //         console.log(xhr.status + ' ' + xhr.statusText + '---' + textStatus);
-        //         console.log(textStatus);
-        //     },
-        //     success     : function(response) {
-        //         if (true === response)
-        //         {
-        //             $(".wpstg_beta_notice").slideUp("slow");
-        //             return true;
-        //         }
-        //
-        //         alert(
-        //             "Unexpected message received. This might mean the data was not saved " +
-        //             "and you might see this message again"
-        //         );
-        //     },
-        //     statusCode  : {
-        //         404: function() {
-        //             alert("Something went wrong; can't find ajax request URL!");
-        //         },
-        //         500: function() {
-        //             alert("Something went wrong; internal server error while processing the request!");
-        //         }
-        //     }
-        // });
     })
 });
