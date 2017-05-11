65f959ba12737c83c02027c03e13e2dc2b63ce0a
René Hermenau <admin@xsimulator.net>
2017-05-11 19:20:57 +0200

    add pro and free switch when plugin is activated

--- wp-staging-pro.php
+++ wp-staging-pro.php
@@ -1,28 +1,14 @@
 <?php
 
 /**
- * Plugin Name: WP Staging
- * Plugin URI: https://wordpress.org/plugins/wp-staging
+ * Plugin Name: WP Staging Pro
+ * Plugin URI: https://wp-staging.com
  * Description: Create a staging clone site for testing & developing
  * Author: WP-Staging, René Hermenau, Ilgıt Yıldırım
  * Author URI: https://wordpress.org/plugins/wp-staging
  * Version: 2.0.3
  * Text Domain: wpstg
  * Domain Path: /languages/
-
- *
- * WP-Staging is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * WP-Staging is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Staging. If not, see <http://www.gnu.org/licenses/>.
  *
  * @package WPSTG
  * @category Core
