2cb5cb048f7ace002d931d32a7a304d7263e5382
René Hermenau <info@mashshare.net>
2018-01-29 17:55:24 +0100

    new notice

--- apps/Backend/views/_includes/messages/wp-version-compatible-message.php
+++ apps/Backend/views/_includes/messages/wp-version-compatible-message.php
@@ -2,11 +2,11 @@
     <p>
         <?php
         echo sprintf( __(
-                        'You are using a version of WP Staging which has not been tested with your WordPress version %2$s.' .
-                        '<br/> As WP Staging is using crucial DB and file functions it\'s important that you are using a ' .
+                        '<strong>Watch out:</strong> This version of WP Staging has not been tested with your WordPress version %2$s.' .
+                        '<br/>As WP Staging is using crucial DB and file functions it\'s important that you are using a ' .
                         'WP Staging version <br> which has been verified to be working with your WordPress version. ' .
-                        'You risk unexpected results up to data lose if you do not so. ' .
-                        '<p>Please look at <a href="%1$s" target="_blank">%1$s</a> for the latest WP Staging version.', 'wpstg'), 'https://wp-staging.com', get_bloginfo( 'version' ) 
+                        'You risk unexpected data lose if you do not so! ' .
+                        '<p><strong>Get the latest WP Staging plugin from <a href="%1$s" target="_blank">https://wp-staging.com</a>.</strong>', 'wpstg'), 'https://wp-staging.com', get_bloginfo( 'version' ) 
         );
         ?>
     </p>
