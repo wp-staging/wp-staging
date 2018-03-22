7e62832d4a4c94548d9ad1ec631222965f246d0b
René Hermenau <admin@xsimulator.net>
2018-03-22 15:32:03 +0100

    better rating notice

--- apps/Backend/views/_includes/messages/rating.php
+++ apps/Backend/views/_includes/messages/rating.php
@@ -1,29 +1,31 @@
-<div class="wpstg_fivestar updated" style="box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);">
-    <p>
-        Awesome, you've been using <strong>WP Staging </strong> for more than 1 week.
-        May I ask you to give it a <strong>5-star</strong> rating on Wordpress?
+<div class="wpstg_fivestar updated" style="box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);border-left:none;background-color:#59a7f7;color:white;">
+    <p><?php _e(' Awesome, you\'ve been using <strong>WP Staging </strong> for more than 1 week.
+        May I ask you to give it a <strong>5-star</strong> rating on Wordpress?', 'wpstg'); ?>
         <br><br>
-        P.S. Looking for a way to copy plugins and theme files from staging to live site? Try out <a href="https://wp-staging.com" target="_blank">WP Staging Pro</a> <br>
-    </p>
-
+        <?php echo sprintf(__('P.S. Looking for a way to migrate the staging site database and copy plugins and theme files from staging to live site?<br/>
+           Try out <a href="%1$s" target="_blank" style="color:white;font-weight:bold;">WP Staging Pro</a>
+                ', 'wpstg'), 'https://wp-staging.com/?utm_source=wpstg_admin&utm_medium=rating_screen&utm_campaign=admin_notice');?>
+         <br>
+      </p>
     <p>
-        <strong>Regards,<br>René Hermenau</strong>
+        Cheers,<br>René Hermenau
     </p>
 
     <ul>
         <li>
-            <a href="https://wordpress.org/support/plugin/wp-staging/reviews/?filter=5#new-post" class="thankyou" target="_new" title="Ok, you deserved it" style="font-weight:bold;">
-                Ok, you deserved it
+            <a href="https://wordpress.org/support/plugin/wp-staging/reviews/?filter=5#new-post" class="thankyou button" target="_new" title="Ok, you deserved it" style="font-weight:bold;">
+                <?php _e('Ok, you deserved it','wpstg')?>
             </a>
         </li>
         <li>
             <a href="javascript:void(0);" class="wpstg_hide_rating" title="I already did" style="font-weight:bold;">
                 I already did
+                <?php _e('I already did','wpstg')?>
             </a>
         </li>
         <li>
             <a href="javascript:void(0);" class="wpstg_hide_rating" title="No, not good enough" style="font-weight:bold;">
-                No, not good enough
+                <?php _e('No, not good enough','wpstg')?>
             </a>
         </li>
     </ul>
