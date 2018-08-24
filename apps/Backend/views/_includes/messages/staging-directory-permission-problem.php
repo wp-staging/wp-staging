41eceaaef065a035d69472ad08dc3cac49ea72a7
René Hermenau <admin@xsimulator.net>
2018-08-24 15:23:01 +0200

    show real php user

--- apps/Backend/views/_includes/messages/staging-directory-permission-problem.php
+++ apps/Backend/views/_includes/messages/staging-directory-permission-problem.php
@@ -4,8 +4,8 @@
         <?php echo sprintf(__('WP Staging Folder Permission error:</strong>
         %1$s is not write and/or readable.
         <br>
-        Check if the folder <strong>%1$s</strong> is writeable by php user www-data.
-        File permissions should be chmod 755 or 777.','wpstg'), ABSPATH );
+        Check if the folder <strong>%1$s</strong> is writeable by php user %2$s or www-data .
+        File permissions should be chmod 755 or 777.','wpstg'), ABSPATH, getenv('USERNAME') ?: getenv('USER') );
         ?>
     </p>
 </div>
