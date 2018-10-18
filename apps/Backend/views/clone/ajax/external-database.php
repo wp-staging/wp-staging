74f6b84d501462625d488c50e53b32816cf5fab3
René Hermenau <admin@xsimulator.net>
2018-10-18 19:46:13 +0200

    add utm parameter to url

--- apps/Backend/views/clone/ajax/external-database.php
+++ apps/Backend/views/clone/ajax/external-database.php
@@ -3,22 +3,22 @@
     Leave User and Password empty to clone the staging site to the current main database.', 'wp-staging'); ?></p>
 <table cellspacing="0" id="wpstg-external-db">
     <tbody>
-        <tr><th>Server</th><td><input type="text" name="wpstg_db_server" id="wpstg_db_server" value="" title="wpstg_db_server" placeholder="localhost" autocapitalize="off">
+        <tr><th>Server</th><td><input type="text" name="wpstg_db_server" id="wpstg_db_server" value="" title="wpstg_db_server" placeholder="localhost" autocapitalize="off" readonly>
             </td></tr>
-        <tr><th>User</th><td><input type="text" name="wpstg_db_username" id="wpstg_db_username" value="" autocapitalize="off" class="">
+        <tr><th>User</th><td><input type="text" name="wpstg_db_username" id="wpstg_db_username" value="" autocapitalize="off" class="" readonly>
             </td></tr>
-        <tr><th>Password</th><td><input type="password" name="wpstg_db_password" id="wpstg_db_password" class="">
+        <tr><th>Password</th><td><input type="password" name="wpstg_db_password" id="wpstg_db_password" class="" readonly>
             </td></tr>
-        <tr><th>Database</th><td><input type="text" name="wpstg_db_database" id="wpstg_db_database" value="" autocapitalize="off">
+        <tr><th>Database</th><td><input type="text" name="wpstg_db_database" id="wpstg_db_database" value="" autocapitalize="off" readonly>
             </td></tr>
-        <tr><th>Database Prefix</th><td><input type="text" name="wpstg_db_prefix" id="wpstg_db_prefix" value="" placeholder="<?php echo $db->prefix; ?>" autocapitalize="off">
+        <tr><th>Database Prefix</th><td><input type="text" name="wpstg_db_prefix" id="wpstg_db_prefix" value="" placeholder="<?php echo $db->prefix; ?>" autocapitalize="off" readonly>
             </td></tr>
         <tr><th><a href="#" id="wpstg-db-connect">Test Database Connection</a></th><td>
             </td></tr>
     </tbody>
 </table>
 </fieldset>
-<p style="font-weight:bold;background-color:#e6e6e6;padding:15px;"><?php _e('This is WP Staging Pro feature', 'wp-staging'); ?>
+<p style="font-weight:bold;background-color:#e6e6e6;padding:15px;"><?php _e('This is a WP Staging Pro feature', 'wp-staging'); ?>
     <br>
-    <a href="https://wp-staging.com" target="_blank" class="quads-button green wpstg-button" style="border-radius:2px;font-size: 14px;">Get WP Staging Pro</a>
+    <a href="https://wp-staging.com/?utm_source=wp-admin&utm_medium=wp-admin&utm_campaign=db-external&utm_term=db-external" target="_blank" class="quads-button green wpstg-button" style="border-radius:2px;font-size: 14px;">Get WP Staging Pro</a>
 </p>
