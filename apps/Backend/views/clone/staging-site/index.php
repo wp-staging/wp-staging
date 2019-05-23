b64d3b640d05452fef9b85024e4657529be91441
René Hermenau <admin@xsimulator.net>
2019-05-23 16:32:04 +0200

    clean up

---
+++ apps/Backend/views/clone/staging-site/index.php
@@ -0,0 +1,4 @@
+<span class="wpstg-notice-alert" style="margin-top:20px;">
+    <?php echo sprintf(__("This is your staging site. Go to your live site to use that function.", "wp-staging"), 
+            admin_url() . 'admin.php?page=wpstg_clone')?>
+</span>
