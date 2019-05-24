97eb1d373feb3f59fbbf819ad3461a41f6028aa1
René Hermenau <admin@xsimulator.net>
2019-05-24 09:50:38 +0200

    improve test

--- codecept-single.bat
+++ codecept-single.bat
@@ -1,5 +1,6 @@
 p:
 cd P:\wp-staging.com\github\wp-staging-pro
 php codecept.phar run --env single --steps
+php codecept.phar run --env single --steps acceptance 001-cloneCest.php:cloneSite
 
 php codecept.phar run --env single --steps acceptance cloneExternalCest.php
