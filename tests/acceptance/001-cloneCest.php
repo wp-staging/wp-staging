da30e21ec84d988d40040b1cc97fcb63bd54b5ec
René Hermenau <admin@xsimulator.net>
2019-05-24 09:50:43 +0200

    improve test

--- tests/acceptance/001-cloneCest.php
+++ tests/acceptance/001-cloneCest.php
@@ -28,26 +28,6 @@ class cloneCest {
     }
 
     public function cloneSite( AcceptanceTester $I ) {
-        // Delete site if it exists
-        // 
-//        $I->performOn('#staging .wpstg-remove-clone', \Codeception\Util\ActionSequence::build()
-//                ->click('#staging .wpstg-remove-clone')
-//                ->waitForElementVisible( "#wpstg-remove-clone", 7 )
-//                ->click( '#wpstg-remove-clone' )
-//                ->waitForElementNotVisible( '#wpstg-removing-clone', 20 )
-//                ->wait( 2 )
-//                );
-//      
-        //if( $I->canSeeElement( '.wpstg-remove-clone' ) ) {
-//        if( $I->seePageHasElement( '//*[@id="staging"]/a[4]' ) ) {
-//            $I->see( 'staging', '#staging' );
-//            $I->click( '#staging .wpstg-remove-clone' );
-//            $I->waitForElementVisible( "#wpstg-remove-clone", 7 );
-//            $I->click( '#wpstg-remove-clone' );
-//            $I->waitForElementNotVisible( '#wpstg-removing-clone', 20 );
-//            $I->wait( 2 );
-//        }
-
         // Create new Staging site
         $I->see( 'Create New Staging Site' );
         $I->click( '#wpstg-new-clone' );
@@ -70,7 +50,7 @@ class cloneCest {
         $I->fillField( [ 'name' => 'wpstg-pass'], 'password' );
         $I->click( '#wp-submit' );
         //$I->wait( 2 );
-        $I->see( 'Dashboard' );
+        $I->see( 'Just another WordPress site' );
         $I->amOnPage( '/staging/wp-admin/admin.php?page=wpstg_clone' );
         //$I->wait( 2 );
         $I->see( 'This is your staging site. Go to your live site to use that function.', '.wpstg-notice-alert' );
