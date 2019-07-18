c03c6e4cb26e1cef9ec151dd0b5937146aef38b4
René Hermenau <admin@xsimulator.net>
2019-07-18 15:57:34 +0200

    refactor structure of _includes view folder

---
+++ apps/Backend/views/_main/footer.php
@@ -0,0 +1,14 @@
+<div id="wpstg-error-wrapper">
+    <div id="wpstg-error-details"></div>
+</div>
+<div id='wpstg-footer' style="clear:both;padding-top:20px;">
+    <br/>
+    <strong>&nbsp;&nbsp;FAQ</strong><br/>
+    - <a href="https://wp-staging.com/docs/staging-site-redirects-live-site/" target="_blank" rel="external">Can not login to staging site</a>    <br/>
+    - <a href="https://wp-staging.com/docs/fix-white-or-blank-page-after-pushing-fatal-error-500/" target="_blank" rel="external">Staging site returns blank white page</a> <br/>
+    - <a href="https://wp-staging.com/docs/activate-permalinks-staging-site/" target="_blank" rel="external">Activate permalinks on staging site</a> <br/>
+    - <a href="https://wp-staging.com/docs/troubleshooting-try-this-first/" target="_blank" rel="external">More</a><br/><br/>
+    <?php echo __('It still does not work?','wp-staging');?>
+    <br>
+    <?php echo sprintf (__('Open a <a href="%s" target="_blank" rel="external nofollow">support ticket</a> and we will resolve it quickly.', 'wp-staging'), 'https://wp-staging.com/support');?>
+</div>
---
+++ apps/Backend/views/_main/report-issue.php
@@ -0,0 +1,33 @@
+<div class="wpstg-report-issue-form">
+    <div class="wpstg-field">
+        <input placeholder="Enter your email address..." type="email" id="wpstg-report-email" class="wpstg-report-email">
+    </div>
+    <div class="wpstg-field">
+        <textarea rows="3" id="wpstg-report-description" class="wpstg-report-description" placeholder="Describe your issue here..."></textarea>
+    </div>
+    <div class="wpstg-field wpstg-report-privacy-policy">
+        <label for="wpstg-report-syslog">
+            <input type="checkbox" class="wpstg-report-syslog" id="wpstg-report-syslog">
+            <?php echo sprintf(
+                    __('Optional: Submit the <a href="%s" target="_blank">System Log</a>. This helps us to resolve your technical issues.','wp-staging'), 
+                    admin_url().'admin.php?page=wpstg-tools&tab=system_info'
+                    ); ?>
+        </label>
+    </div>
+    <div class="wpstg-field wpstg-report-privacy-policy">
+        <label for="wpstg-report-terms">
+            <input type="checkbox" class="wpstg-report-terms" id="wpstg-report-terms">
+            <?php _e('By submitting, I accept the <a href="https://wp-staging.com/privacy-policy/" target="_blank">Privacy Policy</a> and consent that my email will be stored and processed for the purposes of proving support.', 'wp-staging'); ?>
+        </label>
+    </div>
+    <div class="wpstg-field">
+        <div class="wpstg-buttons">                               
+            <button type="submit" id="wpstg-report-submit" class="wpstg-form-submit button-primary wpstg-button">
+                <?php _e( 'Submit', 'wp-staging' ); ?>
+            </button>
+            <span class="spinner"></span>
+             <a href="#" id="wpstg-report-cancel" class="wpstg-report-cancel">Close</a>
+            <div class="wpstg-clear"></div>
+        </div>
+    </div>
+</div>
