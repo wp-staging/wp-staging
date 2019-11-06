9a3add79354dd9bef91f49cc374634426f7e8264
René Hermenau <admin@xsimulator.net>
2019-11-06 09:59:56 +0100

    Fix spelling

--- apps/Backend/views/notices/wp-version-compatible-message.php
+++ apps/Backend/views/notices/wp-version-compatible-message.php
@@ -5,7 +5,7 @@
                         '<strong>Watch out:</strong> This version of WP Staging has not been tested with your WordPress version %2$s.' .
                         '<br/>As WP Staging is using crucial DB and file functions it\'s important that you are using a ' .
                         'WP Staging version <br> which has been verified to be working with your WordPress version. ' .
-                        'You risk unexpected data lose if you do not so! ' .
+                        'You risk unexpected data loss if you do not so! ' .
                         '<p><strong>Get the latest WP Staging plugin from <a href="%1$s" target="_blank">https://wp-staging.com</a>.</strong>', 'wp-staging'), 'https://wp-staging.com', get_bloginfo( 'version' ) 
         );
         ?>
