0a6a11934a89cf7e05a1abc4c55cd32d82eb5101
Rene Hermenau <rene@wp-staging.com>
2020-03-28 18:18:53 +0100

    DateTime Construct with Format in Consideration

--- src/Service/Adapter/DateTimeAdapter.php
+++ src/Service/Adapter/DateTimeAdapter.php
@@ -1,17 +1,52 @@
 <?php
 
 //TODO PHP7.x; declare(strict_types=1);
+//TODO PHP7.x; type hinting & return types
 
 namespace WPStaging\Service\Adapter;
 
-use DateTime as CoreDateTime;
+use DateTime;
+use RuntimeException;
 
 class DateTimeAdapter
 {
+    const DEFAULT_TIME_FORMAT = 'H:i:s';
+
+    /** @var string */
+    private $dateFormat;
+
+    /** @var string */
+    private $timeFormat;
+
+    // TODO PHP5.6 constant
+    private $genericDateFormats = [
+        // WP Suggested formats
+        'F j, Y',
+        'Y-m-d',
+        'm/d/Y',
+        'd/m/Y',
+        // Commonly used formats
+        'd-m-Y',
+        'm-d-Y',
+        'Y-m-d',
+        'Y/m/d',
+    ];
+
+    public function __construct()
+    {
+        $this->dateFormat = get_option('date_format');
+        $this->timeFormat = get_option('time_format');
+    }
+
+    public function getWPDateTimeFormat()
+    {
+        return $this->dateFormat . ' ' . $this->timeFormat;
+    }
+
     public function getDateTimeFormat()
     {
-        $dateFormat = get_option('date_format');
-        $timeFormat = 'H:i:s';
+        $dateFormat = $this->dateFormat;
+        $timeFormat = self::DEFAULT_TIME_FORMAT;
 
         if (!$dateFormat) {
             $dateFormat = 'Y/m/d';
@@ -22,13 +57,45 @@ class DateTimeAdapter
         return $dateFormat . ' ' . $timeFormat;
     }
 
-    // TODO PHP7.0; public function transformWpDateTimeFormat(DateTime $dateTime): string
     /**
-     * @param CoreDateTime $dateTime
+     * @param DateTime $dateTime
      * @return string
      */
-    public function transformToWpFormat(CoreDateTime $dateTime)
+    public function transformToWpFormat(DateTime $dateTime)
     {
         return $dateTime->format($this->getDateTimeFormat());
+    }
+
+    /**
+     * @param string $value
+     * @return DateTime|null
+     */
+    public function getDateTime($value)
+    {
+        $date = null;
+        foreach ($this->generateDefaultDateFormats() as $format) {
+            $date = DateTime::createFromFormat($format, $value);
+            if ($date) {
+                break;
+            }
+        }
+
+        return $date?: null;
+    }
+
+    // TODO
+    private function generateDefaultDateFormats()
+    {
+        $formats = [
+            'U', // Timestamp
+            $this->getDateTimeFormat(),
+            $this->getWPDateTimeFormat(),
+        ];
+
+        foreach ($this->genericDateFormats as $format) {
+            $formats[] = $format . ' ' . self::DEFAULT_TIME_FORMAT;
+        }
+
+        return $formats;
     }
 }
