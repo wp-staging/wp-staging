ae0b58e6d129ee4223681b4ca31b8d3c902c32eb
Koks-A-Cola <Koks-A-Cola@users.noreply.github.com>
2020-06-15 15:29:42 +0200

    #130 tooltip for unfinished or broken clones added (#143)
    merged

--- src/Backend/views/_main/report-issue.php
+++ src/Backend/views/_main/report-issue.php
@@ -1,20 +1,20 @@
 <div class="wpstg-report-issue-form">
     <div class="wpstg-field">
-        <input placeholder="Enter your email address..." type="email" id="wpstg-report-email" class="wpstg-report-email">
+        <input placeholder="Your email address..." type="email" id="wpstg-report-email" class="wpstg-report-email">
     </div>
     <div class="wpstg-field">
-        <input placeholder="(Optional) Enter your hosting provider..." type="text" id="wpstg-report-hosting-provider" class="wpstg-report-hosting-provider">
+        <input placeholder="Your hosting provider...(optional)" type="text" id="wpstg-report-hosting-provider" class="wpstg-report-hosting-provider">
     </div>
     <div class="wpstg-field">
-        <textarea rows="3" id="wpstg-report-description" class="wpstg-report-description" placeholder="Describe your issue here..."></textarea>
+        <textarea rows="3" id="wpstg-report-description" class="wpstg-report-description" placeholder="Describe your issue here! Optionally, include the login credentials to your WordPress admin so we can help you faster."></textarea>
     </div>
     <div class="wpstg-field wpstg-report-privacy-policy">
         <label for="wpstg-report-syslog">
             <input type="checkbox" class="wpstg-report-syslog" id="wpstg-report-syslog">
             <?php echo sprintf(
-                    __('Optional: Submit the <a href="%s" target="_blank">System Log</a>. This helps us to resolve your technical issues.','wp-staging'),
-                    admin_url().'admin.php?page=wpstg-tools&tab=system_info'
-                    ); ?>
+                __('Optional: Submit the <a href="%s" target="_blank">System Log</a> and your WordPress debug log. This helps us to resolve your technical issues.','wp-staging'),
+                admin_url().'admin.php?page=wpstg-tools&tab=system_info'
+            ); ?>
         </label>
     </div>
     <div class="wpstg-field wpstg-report-privacy-policy">
@@ -29,7 +29,7 @@
                 <?php _e( 'Submit', 'wp-staging' ); ?>
             </button>
             <span class="spinner"></span>
-             <a href="#" id="wpstg-report-cancel" class="wpstg-report-cancel">Close</a>
+            <a href="#" id="wpstg-report-cancel" class="wpstg-report-cancel">Close</a>
             <div class="wpstg-clear"></div>
         </div>
     </div>
