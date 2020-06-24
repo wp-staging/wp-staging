009b20f93f86de0497c6ee39c9e52ff0ca094dca
Koks-A-Cola <Koks-A-Cola@users.noreply.github.com>
2020-06-24 21:16:59 +0200

    #144 added feature for sending debug.log with issue reports (#145)

--- src/Core/Utils/Report.php
+++ src/Core/Utils/Report.php
@@ -5,82 +5,81 @@ namespace WPStaging\Utils;
 use WPStaging\Backend\Modules\SystemInfo;
 use WPStaging\DI\InjectionAware;
 
-class Report extends InjectionAware {
+class Report extends InjectionAware
+{
 
-   /**
-    * Send customer issue report
-    *
-    * @param  string  $email   User e-mail
-    * @param  string  $message User message
-    * @param  integer $terms   User accept terms
-    *
-    * @return array
-    */
-   public function send( $email, $message, $terms, $syslog, $provider = null ) {
-      $errors = array();
+    /**
+     * Send customer issue report
+     *
+     * @param string $email User e-mail
+     * @param string $message User message
+     * @param integer $terms User accept terms
+     *
+     * @return array
+     */
+    public function send($email, $message, $terms, $syslog, $provider = null)
+    {
+        $errors      = array();
+        $attachments = array();
+        $maxFileSize = 512 * 1024;
+        $message     .= "\n\n'Hosting provider: " . $provider;
 
-      if( !empty( $syslog ) ) {
-            $message .= "\n\n'Hosting provider: " . $provider;
+        if ( ! empty($syslog)) {
             $message .= "\n\n'" . $this->getSyslog();
-      }
 
-      if( !filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
-         $errors[] = __( 'Email address is not valid.', 'wp-staging' );
-      } elseif( empty( $message ) ) {
-         $errors[] = __( 'Please enter your issue.', 'wp-staging' );
-      } elseif( empty( $terms ) ) {
-         $errors[] = __( 'Please accept our privacy policy.', 'wp-staging' );
-      } else {
+            $debugLogFile = WP_CONTENT_DIR . '/debug.log';
+            if (filesize($debugLogFile) && filesize($debugLogFile) < $maxFileSize) {
+                $attachments[] = $debugLogFile;
+            }
+        }
 
-         if( false === $this->sendMail( $email, $message ) ) {
-            $errors[] = 'Can not send report. <br>Please send us a mail to<br>support@wp-staging.com';
-            //         $response = wp_remote_post(
-//                 'https://wp-staging.com', array(
-//             'timeout' => 15,
-//             'body' => array(
-//                 'email' => $email,
-//                 'message' => $message,
-//             ),
-//                 )
-//         );
-//
-//         if( is_wp_error( $response ) ) {
-//            $errors[] = sprintf( __( 'Something went wrong: %s', 'wp-staging' ), $response->get_error_message() );
-//         }
-         }
-      }
+        if ( ! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = __('Email address is not valid.', 'wp-staging');
+        } elseif (empty($message)) {
+            $errors[] = __('Please enter your issue.', 'wp-staging');
+        } elseif (empty($terms)) {
+            $errors[] = __('Please accept our privacy policy.', 'wp-staging');
+        } else {
 
-      return $errors;
-   }
+            if (false === $this->sendMail($email, $message, $attachments)) {
+                $errors[] = 'Can not send report. <br>Please send us a mail to<br>support@wp-staging.com';
+            }
+        }
 
-   private function getSyslog() {
+        return $errors;
+    }
 
-      $syslog = new SystemInfo( $this->di );
-      return $syslog->get();
-   }
+    private function getSyslog()
+    {
 
-   /**
-    * send feedback via email
-    *
-    * @return boolean
-    */
-   private function sendMail( $from, $text ) {
+        $syslog = new SystemInfo($this->di);
 
-      $headers = array();
+        return $syslog->get();
+    }
 
-      $headers[] = "From: $from";
-      $headers[] = "Reply-To: $from";
+    /**
+     * send feedback via email
+     *
+     * @return boolean
+     */
+    private function sendMail($from, $text, $attachments)
+    {
 
-      $subject = 'Report Issue!';
+        $headers = array();
 
-      $success = wp_mail( 'support@wp-staging.com', $subject, $text, $headers );
+        $headers[] = "From: $from";
+        $headers[] = "Reply-To: $from";
 
-      if( $success ) {
-         return true;
-      } else {
-         return false;
-      }
-      die();
-   }
+        $subject = 'Report Issue!';
+
+        $success = wp_mail('support@wp-staging.com', $subject, $text, $headers, $attachments);
+
+        if ($success) {
+            return true;
+        } else {
+            return false;
+        }
+        die();
+    }
 
 }
